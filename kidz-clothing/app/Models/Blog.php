3672d93166fdd64cdd5f502bd683834f88e36369
Hassanali027 <shassanali223@gmail.com>
2026-06-29 17:29:20 +0500

    fsdf

--- kidz-clothing/app/Models/Blog.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace App\Models;
-use Illuminate\Database\Eloquent\Model;
-class Blog extends Model
-{
-    protected $fillable = ['title', 'slug', 'description', 'thumbnail', 'author', 'status', 'show_on_home'];
-}
